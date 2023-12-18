6fbe4a8aa3adddff5e8e878a2d4c53d2067d4b71
Aleš Bernik <ales.bernik@gora.si>
2023-12-18 14:34:58 +0100

    don't validate schema when redirecting

--- src/main/java/fi/vm/yti/comments/api/resource/UriResolverResource.java
+++ src/main/java/fi/vm/yti/comments/api/resource/UriResolverResource.java
@@ -81,7 +81,8 @@ public class UriResolverResource implements AbstractBaseResource {
     @Produces({ MediaType.APPLICATION_JSON + ";charset=UTF-8", MediaType.TEXT_PLAIN })
     public Response resolveUri(@Parameter(description = "Resource URI.", required = true, in = ParameterIn.QUERY) @QueryParam("uri") final String uri) {
         final URI resolveUri = parseUriFromString(uri);
-        ensureUriHost(uri, uriProperties.getUriHostAddress());
+        String uriNoScheme = uri.replaceFirst("https?://", "");
+        ensureUriHost(uriNoScheme, uriProperties.getHost() + uriProperties.getContextPath());
         final String uriPath = resolveUri.getPath();
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
@@ -108,8 +109,9 @@ public class UriResolverResource implements AbstractBaseResource {
                                 @Parameter(description = "Filter string (csl) for expanding specific child resources.", in = ParameterIn.QUERY) @QueryParam("expand") final String expand,
                                 @Parameter(description = "Filter string (csl) for expanding specific child resources.", in = ParameterIn.QUERY) @QueryParam("token") final String token,
                                 @Parameter(description = "Resource URI.", required = true, in = ParameterIn.QUERY) @QueryParam("uri") final String uri) {
-        ensureUriHost(uri, uriProperties.getUriHostAddress());
-        final String uriPath = uri.substring((uriProperties.getUriHost()).length());
+        String uriNoScheme = uri.replaceFirst("https?://", "");
+        ensureUriHost(uriNoScheme, uriProperties.getHost() + uriProperties.getContextPath());
+        final String uriPath = uriNoScheme.substring((uriProperties.getHost()).length());
         checkResourceValidity(uriPath);
         final String resourcePath = uriPath.substring(uriProperties.getContextPath().length() + 1);
         final List<String> resourcePathParams = parseResourcePathIdentifiers(resourcePath);
